a85cc3302f801bdbe7132d078abcf1743ca9ebb2
sahraomar <sahraaliwrites@gmail.com>
2024-06-05 16:01:40 -0400

    completed

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // Create the `series` variable
 var series = [0, 1];
-
-while (/* your check here */) {
   // your code here
+
+while (series.length < 10) {
+  // add .pop() method on array twice and assign call to .pop() to a variable
+let LastNumber = series.pop();
+let secondLastNumber = series.pop();
+// initiate sum to show lastNumber + secondLastNumber
+let sum = LastNumber + secondLastNumber;
+//push numners back to array with sum
+series.push(secondLastNumber);
+series.push(LastNumber);
+series.push(sum);
+
 }
 
 // Print the Fibonacci sequence to the console
